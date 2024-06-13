42988e207ebe278f0a93e5dfa47fcca1c5ebcfb5
Chen Baowang(陈保旺) <2806054563@qq.com>
2024-06-13 14:50:39 +0800

    Update MapTools.vue
    修改左下角工具栏

--- src/components/MapTools.vue
+++ src/components/MapTools.vue
@@ -1,21 +1,32 @@
 <template>
-    <div :style="{ display: 'flex' }">
-        <a-card id="buttonCard">
-            <a-space>
-                <a-button type="primary" class="button" >添加图层</a-button>
-                <a-button type="primary" class="button">删除图层</a-button>
-            </a-space>
-        </a-card>
+    <div :style="{ display: 'flex',flexDirection: 'column',gap: '1rem' }">
+        <a-tooltip content="旅行足迹" position="right">
+            <a-switch id="switch"></a-switch>
+        </a-tooltip>
+
+        <div class="toolbox">
+            <a-tooltip content="更多工具" position="bottom">
+                <icon-double-right id="toolsIcon"/>
+            </a-tooltip>
+        </div>
   </div>
 
 </template>
 
 <style scoped>
-    .button {
-        border-radius: 4px;
+    .toolbox {
+        background-color: aliceblue;
+        border-radius: 0.5rem;
+        width: 2.5rem;
+        height: 2rem;
+        display: grid;
+        grid-template-columns: 1fr;
     }
-    #buttonCard {
-        border-radius: 1rem;
+    #toolsIcon{
+        margin: auto;
+        width: 1.5rem;
+        height: 1.5rem;
+        cursor: pointer;
     }
 </style>
 
